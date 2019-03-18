7c43e8c02f8afd4535b9de93872bc0cb02f467da
Tom Martinson <38024425+tommartinson@users.noreply.github.com>
2019-03-18 10:48:53 -0700

    add idle time calculations

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ print(service_times)
 # statistics initialization
 waiting_times = []
 queue_lengths = []
+idle_times = []
+
 
 print("\nEach",model_info[2],"will be serviced by",model_info[0],"\n")
 
@@ -50,19 +52,25 @@ def entity(environment, name, service_station, arrival_time, service_time):
      yield environment.timeout(arrival_time)
 
      # request charging bay resource
-     print(model_info[2],'%s arriving at %s' % (name, environment.now))
+     print(model_info[2],'%s arriving at %3.1f' % (name, environment.now))
      with service_station.request() as request:
          yield request
 
          waiting_time = environment.now - arrival_time
-         print(model_info[2],'%s waiting time %s' % (name, waiting_time))
+         print(model_info[2],'%s waiting time %3.1f' % (name, waiting_time))
          
          # servicing entity
-         print(model_info[2],'%s starting service at %s' % (name, environment.now))
+         print(model_info[2],'%s starting service at %3.1f' % (name, environment.now))
+         idle_times.append(environment.now) 
          yield environment.timeout(service_time)
-         print(model_info[2],'%s leaving at %s' % (name, environment.now))
-
+         
+          
+         print(model_info[2],'%s leaving at %3.1f' % (name, environment.now))
+         idle_times.append(environment.now)
+         
          # collect waiting times
+         
+         
          waiting_times.append(waiting_time)
          queue_lengths.append(len(service_station.queue))
 
@@ -76,11 +84,22 @@ for i in range(len(interarrival_times)):
 
 
 environment.run()
+
+#calculate total idle time
+idle_total = 0
+i = 1
+while (i < len(idle_times)-1):
+  idle_total+=idle_times[i+1]-idle_times[i]
+  i=i+2
+
+#calculate resource utilization %
+
+
 print("\nSummary")
 print ("Minumum queue length =",min(queue_lengths))
 print ("Maximum queue length =",max(queue_lengths))
 
-print("Total idle time for the resource = ")
+print ("Total idle time for the resource = %3.2f minutes" % idle_total)
 print("Resource utilization percent = ")
 
 print ("Total queue time = %3.2f minutes" % sum(waiting_times))
